7cbf2344e30eed154e9e5d28fba605e6e063d012
Aleksandr <73224270+MeynSpain@users.noreply.github.com>
2022-03-25 13:19:18 +0900

    Add files via upload

---
+++ Swing/keybindings/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.awt.font.GlyphMetrics;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Game game = new Game();
+    }
+}
