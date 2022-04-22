eb365bb30099f66302e30a7e9fd7bec4fe25c132
Aleksandr <73224270+MeynSpain@users.noreply.github.com>
2022-04-22 12:17:46 +0900

    Add files via upload

---
+++ assembler/assembler.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float c = 3;
+	float chet = 4;
+	__asm {
+	finit
+	fld c
+	fmul st(0), st(0)
+	fldpi
+	fdivp st(1), st(0)
+	fld chet
+	fdivp st(1), st(0)
+	fst c
+	}
+	cout << c;
+}
